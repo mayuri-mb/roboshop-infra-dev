9b70dc43144cb1d9bab6091f43c602cef1c487e3
mayuri <mayu.reddy10@gmail.com>
2026-03-10 19:52:57 +1100

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 component=$1              #receiving component from bootstrap.sh 
 echo "Installing Ansible..."
 dnf install ansible -y
@@ -10,4 +11,4 @@ cd /home/ec2-user
 git clone https://github.com/mayuri-mb/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
-ansible playbook -e component=$component roboshop.yaml
+ansible-playbook -e component=$component roboshop.yaml

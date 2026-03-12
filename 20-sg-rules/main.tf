7f81b1da5e516a512377cc8e2fa17105c21a1737
mayuri <mayu.reddy10@gmail.com>
2026-03-12 11:16:36 +1100

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -50,7 +50,7 @@ resource "aws_security_group_rule" "mysql_bastion" {
     type = "ingress"
     from_port = 22
     to_port = 22
-    protocal = "tcp"
+    protocol = "tcp"
     source_security_group_id = local.bastion_sg_id
     security_group_id = local.mysql_sg_id
 }

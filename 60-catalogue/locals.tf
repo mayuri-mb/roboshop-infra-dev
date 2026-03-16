3035ee59f4640be79811eaee52e831be33dddaaf
mayuri <mayu.reddy10@gmail.com>
2026-03-16 22:03:28 +1100

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,9 +2,11 @@ locals {
     ami_id = data.aws_ami.joindevops.id
     private_subnet_id = split(",",data.aws_ssm_parameter.private_subnet_ids.value)[0]
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    
     common_tags = {
         Project = var.project
         Environment = var.environment
         Terraform = "true"
     }
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
 }
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -58,3 +58,21 @@ resource "aws_ami_from_instance" "catalogue" {
         local.common_tags
     )
 }
+
+resource "aws_lb_target_group" "catalogue" {
+    name = "${var.project}-${var.environment}-catalogue"
+    port = 8080
+    protoco1 = "HTTP"
+    vpc_id = local.vpc_id 
+
+    health_check {
+        healthy_threshold = 2
+        interval = 10
+        matcher = "200-299"
+        path = /health
+        port = 8080
+        protocol = "HTTP"
+        timeout = 2
+        unhealthy_threshold = 3
+    }
+}

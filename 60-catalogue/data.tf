ae462a30ee9fdb6fb77216afd9f722c59b2c21ec
mayuri <mayu.reddy10@gmail.com>
2026-03-16 21:07:51 +1100

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -24,5 +24,5 @@ data "aws_ssm_parameter" "private_subnet_ids" {
 }   
 
 data "aws_ssm_parameter" "catalogue_sg_id" {
-    name = "/${var.project}/${var.environment}/catalague_sg_id"
+    name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }

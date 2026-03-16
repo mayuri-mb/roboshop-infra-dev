a60cc640cb795a249db124da187ff2aeadaa830f
mayuri <mayu.reddy10@gmail.com>
2026-03-16 21:12:29 +1100

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -1,7 +1,7 @@
 #creating catalogue instance
 resource "aws_instance" "catalogue" {
     ami = local.ami_id
-    instance_type = "t3_micro"
+    instance_type = "t3.micro"
     subnet_id = local.private_subnet_id
     vpc_security_group_ids = [local.catalogue_sg_id]
 

dfa51d7f5a6e009f27e7586f3336cf89bb285050
mayuri <mayu.reddy10@gmail.com>
2026-03-18 16:36:43 +1100

    terraform

--- 50-backend-alb/parameter.tf
+++ 50-backend-alb/parameter.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "backend_alb_listener_arn" {
     name = "/${var.project}/${var.environment}/backend_alb_istener_arn"
     type = "String"
-    value = aws_alb_listener.http.arn
+    value = aws_lb_listener.http.arn    
 }

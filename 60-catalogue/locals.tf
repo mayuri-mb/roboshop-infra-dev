4f50110e9d81407a23c8f4cce6da18730d149640
mayuri <mayu.reddy10@gmail.com>
2026-03-18 16:23:03 +1100

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -9,4 +9,5 @@ locals {
         Terraform = "true"
     }
     vpc_id = data.aws_ssm_parameter.vpc_id.value
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
 }
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -121,4 +121,92 @@ resource "aws_launch_template" "catalogue" {
     )
 }
 
-  
+#autoscaling
+resource "aws_autoscaling_group" "catalogue" {
+    name = "${var.project}-${var.environment}-catalogue"
+    max_size = 10
+    min_size = 1
+    health_check_grace_period = 120
+    health_check_type = "ELB"
+    desired_capacity = 1
+    force_delete = false
+
+    launch_template {
+        id = aws_launch_template.catalogue.id
+        version = "$Latest"
+    }
+
+    vpc_zone_identifier = [local.private_subnet_id]   
+    target_group_arns = [aws_lb_target_group.catalogue.arn]
+
+    instance_refresh {
+        stratergy = "Rolling"
+        preference {
+            min_healthy_percentage = 50
+        }
+        triggers = ["launch_template"]
+    }
+
+    dynamic "tag" {
+     for_each = merge(
+        {
+            Name = "${var.project}-${var.environmant}-catalogue"
+        },
+        local.common_tags
+     )
+     content {  
+        key = each.tag
+        value = each.value
+        propagate_at_launch = true
+     }   
+    }   
+
+    #within 15mins autoscaling should be succesful
+    timeouts {
+        delete = "15m"
+    }
+}
+
+#autoscaling policy
+resource "aws_autoscaling_policy" "catalogue" {
+  name = "${var.project}-${var.environment}-catalogue"
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+    predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 70.0
+  }
+
+}  
+
+#lb listener rule
+resource "aws_lb_listener" "catalogue" {
+    listener_arn = local.backend_alb_listener_arn
+    priority = 10
+
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.catalogue.arn
+    }
+    condition {
+        host_headers {
+            values = ["catalogue.backend-alb-${var.environment}.${var.domain_name}"]
+        }
+    }
+}
+
+#delete the instance
+#this depends on target group
+resource "terraform_data" "catalogue_delete" {
+    triggers_replace = [
+        aws_instance.catalogue.id
+    ]
+
+    depends_on = [aws_autoscaling_policy.catalogue]  
+    
+    provisioner "local-exec" {
+        command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
+    }
+}

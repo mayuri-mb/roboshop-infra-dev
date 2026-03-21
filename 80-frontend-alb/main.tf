5f3c2524cb0b11a50a41916c18c1d7c69e796f11
mayuri <mayu.reddy10@gmail.com>
2026-03-21 21:31:25 +1100

    terraform

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -38,7 +38,7 @@ resource "aws_lb_listener" "https" {
 #route53 record for frontend_alb
 resource "aws_route53_record" "frontend_alb" {
     zone_id = var.zone_id
-    name   =  "*.${var.environment}.${var.domain_name}"
+    name   =  "*.${var.domain_name}"    #*.daws-88s.online
     type = "A"
 
     #load balancer details

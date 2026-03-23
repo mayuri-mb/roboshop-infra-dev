34e58cd7f65de2866b2d301bb7767cd87d0b30f6
mayuri <mayu.reddy10@gmail.com>
2026-03-23 14:50:41 +1100

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -3,18 +3,19 @@ variable "component" {
         catalogue = {
             rule_priority = 10
         }
-        user = {
-            rule_priority = 20
-        }
-        cart = {
-            rule_priority = 30   
-        }
-        shipping = {
-            rule_priority = 40   
-        }
-        payment = {
-            rule_priority = 50   
-        }
+        # user = {
+        #     rule_priority = 20
+        # }
+        # cart = {
+        #     rule_priority = 30   
+        # }
+        # shipping = {
+        #     rule_priority = 40   
+        # }
+        # payment = {
+        #     rule_priority = 50   
+        # }
+        #this is attaching to frontend alb, there is only one component there
         frontend = {
             rule_priority = 10
         }

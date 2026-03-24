691d016027b6bc142ac9e9c7f37646d41f4d70ab
mayuri <mayu.reddy10@gmail.com>
2026-03-24 21:43:18 +1100

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -1,24 +1,24 @@
 variable "components" {
     default = {
         # backend components are attaching to backend ALB
-        # catalogue = {
-        #     rule_priority = 10
-        # }
-        # user = {
-        #     rule_priority = 20
-        # }
-        # cart = {
-        #     rule_priority = 30   
-        # }
-          shipping = {
+        catalogue = {
+            rule_priority = 10
+        }
+        user = {
+            rule_priority = 20
+        }
+        cart = {
+            rule_priority = 30   
+        }
+        shipping = {
             rule_priority = 40   
-         }
-        # payment = {
-        #     rule_priority = 50   
-        # }
+        }
+        payment = {
+            rule_priority = 50   
+        }
         # this is attaching to frontend alb, there is only one component there
-        #   frontend = {
-        #       rule_priority = 10 
-        #   }
+        frontend = {
+            rule_priority = 10 
+        }
     }
 }

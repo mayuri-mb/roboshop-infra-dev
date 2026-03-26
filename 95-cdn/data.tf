46cad3f9426d3dda7ca3fd3cf658bac07c2a2eac
mayuri <mayu.reddy10@gmail.com>
2026-03-26 11:17:12 +1100

    terraform

---
+++ 95-cdn/data.tf
@@ -0,0 +1,11 @@
+resource "aws_cloudfront_cache_policy" "cachingDisabled" {
+    name = "Managed-CachingDisabled"
+}
+
+resource "aws_cloudfront_cache_policy" "cachingOptimized" {
+    name = "Managed-CachingOptimized"
+}
+
+data "aws_ssm_parameter" "acm_certificate_arn" {
+  name = "/${var.project}/${var.environment}/frontend_alb_certificate_arn"
+}

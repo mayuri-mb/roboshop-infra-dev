91c3d645da0f3ee2115f0d180daa2931035e27d6
mayuri <mayu.reddy10@gmail.com>
2026-03-29 12:44:00 +1100

    terraform

--- 20-sg-rules/local.tf
+++ 20-sg-rules/local.tf
@@ -17,4 +17,5 @@ locals {
 
     frontend_sg_id = data.aws_ssm_parameter.frontend_sg_id.value
     frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
+    openvpi_sg_id = data.aws_ssm_parameter.openvpn_sg_id.value
 }
